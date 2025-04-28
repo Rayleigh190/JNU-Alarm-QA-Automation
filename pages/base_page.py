112ac14dc80ea2582c2c3eae75b47c4f2d6bc99a
rayleigh <rayleigh190@gmail.com>
2025-04-28 16:13:56 +0900

    [REFACTOR] iOS 스크롤 스크립트를 scroll에서 swipe로 리팩토링

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,6 @@
 import time
 from typing import Tuple
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from appium.webdriver.common.appiumby import AppiumBy
@@ -35,19 +35,20 @@ class BasePage:
     except:
       return False
   
-  def scroll_to_element(self, locator):
+  def scroll_to_element(self, name):
     if self.driver.capabilities['platformName'] == "Android":
-      self.scroll_into_view(locator=locator, max_scrolls=5, steps=200)
+      self.scroll_into_view_android(name=name, max_scrolls=5, steps=200)
     else:
-      self.driver.execute_script("mobile: scroll", {"direction": "down", "predicateString": f'name == "{locator}"'})
+      # self.driver.execute_script("mobile: scroll", {"direction": "down", "predicateString": f'name == "{name}"'})
+      self.scroll_into_view_ios(locator=(AppiumBy.XPATH, f'//XCUIElementTypeOther[@name="{name}"]/following-sibling::XCUIElementTypeSwitch'))
   
-  def scroll_into_view(self, locator, max_scrolls=10, steps=55):
+  def scroll_into_view_android(self, name, max_scrolls=10, steps=55):
     scrollable = 'new UiScrollable(new UiSelector().scrollable(true))'
     for _ in range(max_scrolls):
       try:
         element = self.driver.find_element(
           AppiumBy.ANDROID_UIAUTOMATOR,
-          f'new UiSelector().description("{locator}")'
+          f'new UiSelector().description("{name}")'
         )
         return element
       except NoSuchElementException:
@@ -60,4 +61,19 @@ class BasePage:
         except Exception as e:
           print("더 이상 스크롤 불가", e)
           break
-    raise Exception(f"{max_scrolls}회 스크롤 동안 description이 '{locator}'인 element를 찾지 못함.")
+    raise Exception(f"{max_scrolls}회 스크롤 동안 description이 '{name}'인 element를 찾지 못함.")
+  
+  def swipe_up(self, velocity=1000, percent=0.7):
+    self.driver.execute_script("mobile: swipe", {
+      "direction": "up",
+      "velocity": velocity,
+      "percent": percent
+    })
+
+  def scroll_into_view_ios(self, locator):
+    for _ in range(5):
+      try:
+        return self.get_element(locator, timeout=2)
+      except TimeoutException:
+        self.swipe_up(velocity=300, percent=0.5)
+    raise Exception(f"스크롤 후에 {locator} element를 찾지 못함.")
